b098cc629d6027f06d3f2df48f8b01b3a35bb3f7
Olivier Boudeville <olivier.boudeville@esperide.com>
2019-05-05 13:48:08 +0200

    Added.

---
+++ src/mobile.hrl
@@ -0,0 +1,46 @@
+% Copyright (C) 2019-2019 Olivier Boudeville
+%
+% This file is part of the Ceylan-Mobile library.
+%
+% This library is free software: you can redistribute it and/or modify
+% it under the terms of the GNU Lesser General Public License or
+% the GNU General Public License, as they are published by the Free Software
+% Foundation, either version 3 of these Licenses, or (at your option)
+% any later version.
+% You can also redistribute it and/or modify it under the terms of the
+% Mozilla Public License, version 1.1 or later.
+%
+% This library is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+% GNU Lesser General Public License and the GNU General Public License
+% for more details.
+%
+% You should have received a copy of the GNU Lesser General Public
+% License, of the GNU General Public License and of the Mozilla Public License
+% along with this library.
+% If not, see <http://www.gnu.org/licenses/> and
+% <http://www.mozilla.org/MPL/>.
+%
+% Author: Olivier Boudeville [olivier (dot) boudeville (at) esperide (dot) com]
+% Creation date: Sunday, March 24, 2019.
+
+
+
+% Stores information about a received SMS.
+-record( received_sms, {
+
+	% The mobile number of the SMS sender (as a binary):
+	sender_number :: bin_mobile_number(),
+
+	% The encoding used for the transmitted text:
+	encoding :: encoding(),
+
+	% The corresponding text:
+	text :: bin_sms_message(),
+
+	% The GSM network message reference:
+	message_reference :: sms_tpmr(),
+
+	% The best sending timestamp determined for this message:
+	timestamp :: sms_timestamp() } ).
---
+++ tests/mobile_read_test.erl
@@ -0,0 +1,68 @@
+% Copyright (C) 2019-2019 Olivier Boudeville
+%
+% This file is part of the Ceylan-Mobile library.
+%
+% This library is free software: you can redistribute it and/or modify
+% it under the terms of the GNU Lesser General Public License or
+% the GNU General Public License, as they are published by the Free Software
+% Foundation, either version 3 of these Licenses, or (at your option)
+% any later version.
+% You can also redistribute it and/or modify it under the terms of the
+% Mozilla Public License, version 1.1 or later.
+%
+% This library is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+% GNU Lesser General Public License and the GNU General Public License
+% for more details.
+%
+% You should have received a copy of the GNU Lesser General Public
+% License, of the GNU General Public License and of the Mozilla Public License
+% along with this library.
+% If not, see <http://www.gnu.org/licenses/> and
+% <http://www.mozilla.org/MPL/>.
+%
+% Author: Olivier Boudeville [olivier (dot) boudeville (at) esperide (dot) com]
+% Creation date: Sunday, March 24, 2019.
+
+
+
+% Allows to test the Ceylan-Mobile services regarding the reading of SMS.
+%
+% Note that with the 'dummy' Gammu model, TPMR references might be always 255.
+%
+-module(mobile_read_test).
+
+
+-export([ run/0 ]).
+
+
+
+run() ->
+
+	test_facilities:start( ?MODULE ),
+
+	test_facilities:display(
+	  "Testing the Ceylan-Mobile service regarding the reading of SMS." ),
+
+	% Not mobile:start_link(), as here we want to survive a crash of the mobile
+	% service (i.e. to be able to handle failures explicitly, as messages
+	% received by this test process):
+	%
+	mobile:start(),
+
+	case mobile:read_all_sms() of
+
+		[] ->
+			test_facilities:display( "No SMS to read found." );
+
+		SMSList ->
+			test_facilities:display( "~B SMS read: ~s", [ length( SMSList ),
+				text_utils:strings_to_string( [ mobile:received_sms_to_string( S )
+												|| S <- SMSList ] ) ] )
+
+	end,
+
+	mobile:stop(),
+
+	test_facilities:stop().
